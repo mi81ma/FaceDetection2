9117d884fa80ec8679f2f46699ac52d89e95c75f
masato <masato@m-mbp.local>
2018-10-24 13:25:32 +0800

    Adjust Red Box Position

--- FaceDetection2/ViewController.swift
+++ FaceDetection2/ViewController.swift
@@ -14,18 +14,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        guard let image = UIImage(named: "sample1") else
+        guard let image = UIImage(named: "sample3") else
         { return }
 
         let imageView = UIImageView(image: image)
         imageView.contentMode = .scaleAspectFit
 
-        let scaledHeight = view.frame.width / image.size.width
+        let scaledHeight = view.frame.width / image.size.width * image.size.height
 
+        imageView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: scaledHeight)
 
-        imageView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 200)
-
-//        imageView.backgroundColor = .blue
 
         view.addSubview(imageView)
 
@@ -39,31 +37,44 @@ class ViewController: UIViewController {
             print(req)
 
             req.results?.forEach({ (res) in
-                print(res)
 
-                guard let faceObservation = res as?
-                    VNFaceObservation else { return }
+                DispatchQueue.main.async {
+                    guard let faceObservation = res as?
+                        VNFaceObservation else { return }
 
-                let x = self.view.frame.width * faceObservation.boundingBox.origin.x
-                let y = self.view.frame.height * faceObservation.boundingBox.origin.y
+                    let x = self.view.frame.width * faceObservation.boundingBox.origin.x
+                    let height = scaledHeight * faceObservation.boundingBox.height
+                    let y = scaledHeight * (1 - faceObservation.boundingBox.origin.y) - height
+                    let width = self.view.frame.width * faceObservation.boundingBox.width
 
-                let redView = UIView()
-                redView.backgroundColor = .red
-                redView.alpha = 0.4
-                redView.frame = CGRect(x: x, y: 0, width: 100, height: 100)
-                self.view.addSubview(redView)
 
-                print(faceObservation.boundingBox)
+
+                    let redView = UIView()
+                    redView.backgroundColor = .red
+                    redView.alpha = 0.4
+                    redView.frame = CGRect(x: x, y: y, width: width, height: height)
+                    self.view.addSubview(redView)
+
+                    print(faceObservation.boundingBox)
+                }
+
+
             })
 
         }
 
         guard let cgImage = image.cgImage else { return }
-        let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
-        do {
-            try handler.perform([request])
-        } catch let reqErr {
-            print("Failed to perform request:", reqErr)
+
+        DispatchQueue.global(qos: .background).async {
+
+            let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+            do {
+                try handler.perform([request])
+            } catch let reqErr {
+                print("Failed to perform request:", reqErr)
+
+        }
+
 
         }
 
